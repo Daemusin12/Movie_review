a194b92f70a817167962503095a2c000377e99e2
Daemusin12 <engrleonelfelezario812@gmail.com>
2023-04-16 13:22:05 +0800

    set addresses for users signup keys

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :address_region_id, :address_province_id, :address_city_id, :address_barangay_id, :address])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
   end
 
