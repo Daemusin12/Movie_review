d4deb45eca8811e0c11547e2115d542633aa0617
Daemusin12 <engrleonelfelezario812@gmail.com>
2023-04-17 18:28:55 +0800

    create short_url_generator method in movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -15,4 +15,12 @@ class Movie < ApplicationRecord
     self.reviews.sum(:rating) / self.reviews.count.to_f
   end
 
+  def short_url_generator
+    range = [*'0'..'9',*'A'..'Z',*'a'..'z']
+    url = Array.new(7){ range.sample }.join
+    until Movie.find_by(short_url: url) == nil
+      url = Array.new(7){ range.sample }.join
+    end
+    self.short_url = url
+  end
 end
