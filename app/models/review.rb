1e86149eaf09c0890bb3d345be810b2385e2d634
Daemusin12 <engrleonelfelezario812@gmail.com>
2023-04-17 16:14:06 +0800

    edit  review ratings validation to limit to 1-5, added scope for reviews descending order

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,8 @@ class Review < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true
-  validates :rating, presence: true
+  validates :rating, presence: true, numericality: { in: 1..5 }
+
+  scope :top_ratings, -> { order(rating: :desc) }
+
 end
